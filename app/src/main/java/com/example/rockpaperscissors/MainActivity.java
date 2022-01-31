e9fd72b0cfcbd8554a012be08fe7da99a8769e26
TJordan <Tjordan7@stu.jsu.edu>
2022-01-31 00:08:58 -0600

    Finish project

--- app/src/main/java/com/example/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.java
@@ -4,20 +4,53 @@ import java.util.concurrent.ThreadLocalRandom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private int playerScore = 0;
+    private int aiScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.rockButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                processRound(Weapon.ROCK);
+            }
+        });
+        findViewById(R.id.paperButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                processRound(Weapon.PAPER);
+            }
+        });
+        findViewById(R.id.scissorsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                processRound(Weapon.SCISSORS);
+            }
+        });
+
     }
 
-    private String findOutcome (Weapon playerWeapon) {
-        int aiWeaponSelector = ThreadLocalRandom.current().nextInt(1, 3 + 1);
+    public void processRound(Weapon playerWeapon) {
+        TextView scoresString = findViewById(R.id.scores);
+        TextView playerWeaponString = findViewById(R.id.playerWeapon);
+        TextView aiWeaponString = findViewById(R.id.aiWeapon);
+        TextView outcomeString = findViewById(R.id.outcome);
         Weapon aiWeapon = null;
-        String winner = null;
+        Weapon winningWeapon = null;
+        String winnerBlurb = "";
 
+
+        int aiWeaponSelector = ThreadLocalRandom.current().nextInt(1, 3 + 1);
         if (aiWeaponSelector == 1) {
             aiWeapon = Weapon.ROCK;
         } else if (aiWeaponSelector == 2) {
@@ -25,6 +58,37 @@ public class MainActivity extends AppCompatActivity {
         } else if (aiWeaponSelector == 3) {
             aiWeapon = Weapon.SCISSORS;
         }
+
+        String outcome = findOutcome(aiWeapon, playerWeapon);
+        if (outcome == "player") {
+            playerScore++;
+            winningWeapon = playerWeapon;
+            winnerBlurb = "Player wins ... ";
+        } else if (outcome == "computer") {
+            aiScore++;
+            winningWeapon = aiWeapon;
+            winnerBlurb = "Computer wins ... ";
+        } else if (outcome == "draw") {
+            winnerBlurb = "Draw!";
+        }
+
+        if (winningWeapon == Weapon.ROCK) {
+            winnerBlurb += "Rock crushes scissors!";
+        } else if (winningWeapon == Weapon.PAPER) {
+            winnerBlurb += "Paper covers rock!";
+        } else if (winningWeapon == Weapon.SCISSORS) {
+            winnerBlurb += "Scissors cuts paper!";
+        }
+
+        scoresString.setText("Player: " + String.valueOf(playerScore) + ", Computer: " + String.valueOf(aiScore));
+        playerWeaponString.setText("Player's weapon: " + playerWeapon.toString());
+        aiWeaponString.setText("Computer's weapon: " + aiWeapon.toString());
+        outcomeString.setText(winnerBlurb);
+
+    }
+
+    private String findOutcome (Weapon aiWeapon, Weapon playerWeapon) {
+        String winner = null;
 
         if (playerWeapon == aiWeapon) {
             winner = "draw";
